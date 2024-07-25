6f9b64ad27f6c22c4d33e785bf0d3372d837dce3
umashini2002 <umashini2002@gmail.com>
2024-07-26 01:33:47 +0530

    commit

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! hi");
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            double total = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+            total += numbers[i];
+            }
+            double average = total / numbers.Length;
+            Console.WriteLine($"total ={total},average = {average}");
         }
     }
 }
