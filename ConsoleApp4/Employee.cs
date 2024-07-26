6f420ff678d74ec4c4539f76fa2901a6eead7822
umashini2002 <umashini2002@gmail.com>
2024-07-26 06:13:24 +0530

    commit

---
+++ ConsoleApp4/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double BasaicSalary {  get; set; }
+        public Employee(string Afirstname, string Alastname, double Abasicsalary)
+        {
+            FirstName = Afirstname;
+            LastName = Alastname;
+            BasaicSalary = Abasicsalary;
+        }
+        public void print(string Afirstname,string Alastname,double Abasicsalary)
+        {
+            Console.WriteLine($"Name = {FirstName} {LastName},Basic Salary = {BasicSalary}");
+        }
+
+    }
+}
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! hi");
+            Employee employee1 = new Employee("Chamil", "Jeewantha", 250000.00);
+            Employee employee2 = new Employee("Umashini", "Sureshkumar", 40000.25);
+            employee1.print(employee1.FirstName, employee1.LastName,employee1.BasaicSalary);
         }
     }
 }
